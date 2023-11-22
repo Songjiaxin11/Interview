130dfbfe873450b98fc4a707bd1a284128526c64
Songjiaxin11 <1003414719@qq.com>
2023-11-22 23:51:43 +0800

    add .gitignore

--- branch_add.sh
+++ branch_add.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+## 直接创建分支并推送远程
+# git checkout -b test001
+# git push origin test001
+## 删除本地分支
+# git branch -d test001
+## 删除某个远程分支
+# git push origin --delete branch_name
+## 更新完某个bash文件后, 需要执行下面的命令使其生效
+# chmod +x branch_add.sh
+##然后运行
+# ./branch_add.sh
 # 通过读取csv创建分支
 ###################################################
 # CSV 文件路径
