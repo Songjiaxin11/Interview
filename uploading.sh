ec93305e4184883d591896944145db89a4b9a1d5
Songjiaxin11 <1003414719@qq.com>
2023-11-22 22:54:43 +0800

    🎉Initialize

--- uploading.sh
+++ uploading.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# *!!!!!!注意, 一定要先把main分支的代码提交到远程仓库, 否则会出现无法推送的情况!!!!!!*
 # 远程仓库的名称
 remote="origin"
 
@@ -7,7 +7,12 @@ remote="origin"
 base_branch="main"
 
 # 指定的远程分支列表
-target_remote_branches=("test001" "test002")  # 替换为实际的目标远程分支名称
+# target_remote_branches=("test001" "test002")  # 替换为实际的目标远程分支名称
+
+#从csv读取分支列表
+csv_file="./branchName.csv"
+# 从 CSV 文件中读取目标远程分支名称
+target_remote_branches=($(csvtool -t COMMA col 1 $csv_file))
 
 # 切换到主分支，获取最新的主分支内容
 git checkout "$base_branch"
